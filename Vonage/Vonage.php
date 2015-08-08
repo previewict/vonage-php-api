8bada86dc3713c12cc044f9435c062e452e2a1b9
Shaharia Azam <shaharia.azam@gmail.com>
2015-08-09 04:17:58 +0600

    Vonage API in PHP more simplified manner to use

--- Vonage/Vonage.php
+++ Vonage/Vonage.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Vonage;
 
 use GuzzleHttp\Client;
@@ -7,43 +6,54 @@ use GuzzleHttp\Cookie\CookieJar;
 
 class Vonage
 {
+    protected $apiDomain = 'https://my.vonagebusiness.com';
     private $cookie;
     private $client;
-
+    public $data = array();
     public $auth = array();
+    public $finalUrl;
+    public $restBaseUrl;
 
-    protected $vonageApiUrl = 'https://my.vonagebusiness.com';
-
+    /**
+     * @param $username
+     * @param $password
+     * @param array $options
+     */
     public function __construct($username, $password, $options = array())
     {
-        $this->client = new Client();
+        $this->restBaseUrl = $this->apiDomain . '/presence/rest/';
+        $this->client = new Client(array('base_uri' => $this->restBaseUrl));
         $this->cookie = new CookieJar();
+        $response = $this->client->get($this->apiDomain.'/appserver/rest/user/null',
+            ['cookies' => $this->cookie, 'query' => ['htmlLogin' => $username, 'htmlPassword' => $password]]);
+        $body = $response->getBody()->getContents();
 
-        $response = $this->client->get($this->vonageApiUrl . '/appserver/rest/user/null',
-            [
-                'cookies' => $this->cookie,
-                'query' => ['htmlLogin' => $username, 'htmlPassword' => 'ltqpsmr7']
-            ]
-        );
-
-        if (!empty($response->getBody()->getContents())) {
-            $this->auth = json_decode($response->getBody()->getContents());
+        if (!empty($body)) {
+            $this->auth = json_decode($body);
         }
     }
 
     /**
-     * @param $number
-     * @return bool|string
+     * @param $url
+     * @param array $params
+     * @param array $options
+     * @return array|string
      */
-    public function makeCall($number)
+    public function request($url, $params = array(), $options = array())
     {
         try {
-            $response = $this->client->get($this->vonageApiUrl . '/presence/rest/clicktocall/' . $number,
-                ['cookies' => $this->cookie]);
+            if(isset($params) && !empty($params) && is_array($params)){
+                $response = $this->client->get($url, ['cookies' => $this->cookie, 'query' => http_build_query($params)]);
+                $this->finalUrl = urldecode($this->restBaseUrl . $url . '?' . http_build_query($params));
+            }else{
+                $response = $this->client->get($url, ['cookies' => $this->cookie]);
+                $this->finalUrl = urldecode($this->restBaseUrl . $url);
+            }
 
             return $response->getBody()->getContents();
         } catch (\Exception $e) {
-            return false;
+            return array();
         }
+
     }
 }
--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 require "vendor/autoload.php";
 require "config.php";
 
-$vonage = new \Vonage\Vonage(VONAGE_USERNAME, VONAGE_PASSWORD);
-var_dump($vonage->makeCall('NumberToCall')); die();
+$vonage = new \Vonage\Vonage('vonageUsername', 'VonagePassword');
+$params = array(
+    'start' => date('Y-m-d\TH:i:sP')
+);
+var_dump($vonage->request('callhistory/{VonageExtensionNumber}', $params)); die();
